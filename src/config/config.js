b54f8d72103262933826144b35f7b5e45b680ef7
LiaSoftwareTeam <liatechagrotech@gmail.com>
2025-01-03 15:48:30 -0400

    envs

--- src/config/config.js
+++ src/config/config.js
@@ -7,3 +7,4 @@ export const DB_HOST = process.env.DB_HOST || 'localhost'
 export const DB_PASS = process.env.DB_PASS || '12345678'
 export const DB_USER = process.env.DB_USER || 'darmin'
 export const DB_NAME = process.env.DB_NAME || 'itlasecciones'
+export const DB_PORT = process.env.DB_PORT || 3306
